a74702bb4dc0fa50a4fae3f28f81edfb3d1099e7
Emily Vasconcelos <emily_cvasconcelos@hotmail.com>
2021-08-28 19:57:12 -0300

    WIP

---
+++ pages/RelatorioCovidPage/relatorio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Relatorio = ({ navigation }) => {
+    return(
+        <View>
+            
+        </View>
+    );
+};
+
+export default Relatorio;
